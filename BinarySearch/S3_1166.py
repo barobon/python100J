f8cfe93868a81c93db58e25768b4cf7271988baf
barobon <46195201+barobon@users.noreply.github.com>
2022-01-24 23:04:58 +0900

    1166 1018

---
+++ BinarySearch/S3_1166.py
@@ -0,0 +1,14 @@
+N, L , W, H = map(int, input().split())
+left, right = 0, max(L, W, H)
+
+for _ in range(10000):
+    mid = (left + right) / 2
+    result = (L//mid) * (W//mid) * (H//mid)     # 개수는 무조건 정수여야 하기 때문에 // 사용
+
+    if(result >= N):     # A 값이 작다
+        left = mid
+    else:   # A 값이 크다
+        right = mid
+  
+
+print("%.11f" %(right))
