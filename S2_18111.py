371a1a4f246dface2921fa0329da52f37d76b450
barobon <46195201+barobon@users.noreply.github.com>
2022-02-03 15:27:12 +0900

    working on 18111

---
+++ S2_18111.py
@@ -0,0 +1,27 @@
+import sys
+N, M, B = map(int, sys.stdin.readline().split())
+ground = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+minTime = int(1e11)
+maxHeight = -1
+
+maxH = max(map(max, ground))
+minH = min(map(min, ground))
+for h in range(minH, maxH+1):
+    tmpTime = 0
+    stackNum =0
+    delNum =0
+    for i in range(N):
+        for j in range(M):
+            if(h - ground[i][j] >= 0):
+                stackNum += h - ground[i][j]
+            else:
+                delNum += ground[i][j] - h
+
+    if(B<stackNum-delNum):
+        continue
+    tmpTime = stackNum + delNum * 2
+    if(tmpTime < minTime):
+        minTime = tmpTime
+        maxHeight = h
+print(minTime, maxHeight)
--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-from collections import deque
-
-que = deque([[0, 1]])
-
-while que:
-    p = []
-    p = que.pop()
-    print(p)
-    num = p[0] + 1
-    print(num)
-
-
-print('end')
+import sys
+tmp = list(map(int, sys.stdin.readline().split()))
+print(tmp)
