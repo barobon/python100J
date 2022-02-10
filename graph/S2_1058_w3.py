73885470b02642594a37ec3d787360e3c5d83032
barobon <46195201+barobon@users.noreply.github.com>
2022-02-10 15:19:09 +0900

    Update S2_1058_w3.py

--- graph/S2_1058_w3.py
+++ graph/S2_1058_w3.py
@@ -1,9 +1,29 @@
 import sys
 
+def dfs(node, depth, count):
+    visited[node] = 1
+    print(visited)
+    depth += 1
+    for i in range(N):
+        if visited[i] == 0 and graph[node][i] == 'Y':
+            count += 1
+            if depth < 2:
+                count = dfs(i, depth, count)
+    return count
+
 N = int(input())
 graph = []
-
+friend = [0]*N
 for _ in range(N):
+    #rstrip로 줄바꿈 삭제
     graph.append(list(sys.stdin.readline().rstrip()))
 
-print(graph)
+for i in range(N):
+    depth = 0
+    count = 0
+    visited = [0]*N
+    count = dfs(i, depth, count)
+    print(count)
+    friend[i] = count
+
+print(friend)
